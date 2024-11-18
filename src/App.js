ca023469767fe2cc512f094a142474d723a66c52
Ivan Flitcroft <Ivanflitcroft@gmail.com>
2024-11-18 19:45:58 +0000

    Initial variables set

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [snake, setSnake] = useState([[10,10]])
+  const [food, setFood] = useState([5, 5]);
+  const [direction, setDirection] = useState("RIGHT");
+  const [gameOver, setGameOver] = useState(false);
+  const gridSize = 20;
   return (
     
   );
